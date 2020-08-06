2bc5573382e7cbfb3508ff064f8609bc33a264cf
AsadBek21coder <asadbekegambergenoov21coder@gmail.com>
2020-08-06 22:14:58 +0500

    correcting some mistakes

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 
+async function main () {
 
+	const response =  await fetch('http://localhost:8080/api')
+	
+	const data = await response.json()
+
+	 for ( let i of data ) {
+
+ 	 const li = document.createElement('LI')
+ 	 li.textContent = i
+ 	 myUl.appendChild(li)
+
+ 	 console.log(i)
+	}
+
+	// console.log(response, data)
+
+}
+main()
 
 
 // fetch('http://localhost:8080/api')
