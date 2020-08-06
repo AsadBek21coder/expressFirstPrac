4e4d0d10e60610761d044247ff5f154baaf561a4
AsadBek21coder <asadbekegambergenoov21coder@gmail.com>
2020-08-06 21:57:18 +0500

    creating API

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const PORT = 8080
 
 app.get( '/', (req,res) => {
 	res.send('Hello world')
+	console.log('sent "Hello world"')
+})
+
+app.get('/api', (req,res) => {
+	res.send( [1,2,3] )
+	console.log('Array recieved')
 })
 
 
